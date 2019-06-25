2c35bdd1dc2a01a61721f13dcdc9365776b29e54
Ирина Карбачинская <o.irina.k@gmail.com>
2019-06-25 21:32:25 +0300

    правки в JS

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 var open = document.querySelector(".search-hotel-button");
 var form = document.querySelector(".search-hotel-form");
 
-form.classList.add("hotel-form-hide")
+form.classList.add("hotel-form-hide");
 
 open.addEventListener("click", function (evt) {
   evt.preventDefault();
   form.classList.toggle("hotel-form-hide");
   form.classList.toggle("hotel-form-show");
-}
+})
